1207c5c78029994b12a5a53ed222122aadebb78e
niko bazylev <bazylevnik0@gmail.com>
2023-09-30 08:42:26 +0000

    Create 18 files

---
+++ gravity-simulation/src/App.js
@@ -0,0 +1,14 @@
+//import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>click and push for add object</h1>
+      <canvas id="canvas"></canvas>
+      <button>START</button>
+    </div>
+  );
+}
+
+export default App;
